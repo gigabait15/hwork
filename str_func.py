fcdaa7e99d2f8444fbf0211d2618a6676247c202
gigabait <Workkerbecov@yandex.ru>
2023-04-28 11:20:58 +0300

    add upper text and capitalize text and all docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def s_upper(value):
-    """retur text"""
+    """:return upper text"""
     return str(value).upper()
+
+def s_capitalize(value):
+    """:return capitalize text"""
+    return str(value).capitalize()
